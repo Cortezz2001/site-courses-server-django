59aa26a5a006c3e71dd0fc8a55b2916be408c0da
Maxim Fedkevich <cortezzcloud@gmail.com>
2024-05-11 00:09:16 +0500

    banners app

--- courses_backend/courses_backend/settings.py
+++ courses_backend/courses_backend/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'courses.apps.CoursesConfig',
     'mentors.apps.MentorsConfig',
     'events.apps.EventsConfig',
+    'banners.apps.BannersConfig',
     'rest_framework',
     'rest_framework.authtoken',
     'djoser',
--- courses_backend/courses_backend/urls.py
+++ courses_backend/courses_backend/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('api/v1/mentors/', include("mentors.urls")),
     path('api/v1/events/', include("events.urls")),
     path('api/v1/courses/', include("courses.urls")),
+    path('api/v1/banners/', include("banners.urls")),
     path('api/v1/auth/', include('auth_system.urls')),
     path('admin/', admin.site.urls),
 ]
